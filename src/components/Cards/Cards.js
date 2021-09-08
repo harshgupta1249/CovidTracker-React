55b9656f182af171beaa40cf3544aca8198f16cf
Harsh <harshgupta1249@gmail.com>
2021-09-08 21:06:16 +0530

    classnames added

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
+import classnames from 'classnames';
 
 import styles from './Cards.modules.css';
 
@@ -14,7 +15,7 @@ const Cards = ({ data : { confirmed, recovered, deaths, lastUpdate }} ) => {
     return ( 
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={classnames(styles.card, styles.infected)}>
                 <CardContent>
                     <Typography color="textSecondary" gutterBottom>Infected</Typography>
                     <Typography variant="h5">
@@ -25,7 +26,7 @@ const Cards = ({ data : { confirmed, recovered, deaths, lastUpdate }} ) => {
                 </CardContent>
                 </Grid>
 
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={classnames(styles.card, styles.recovered)}>
                 <CardContent>
                     <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                     <Typography variant="h5">
@@ -36,7 +37,7 @@ const Cards = ({ data : { confirmed, recovered, deaths, lastUpdate }} ) => {
                 </CardContent>
                 </Grid>
 
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={classnames(styles.card, styles.deaths)}>
                 <CardContent>
                     <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                     <Typography variant="h5">
