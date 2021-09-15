3c210ca50980b67890e2824ca913851e448f8d74
Harsh <harshgupta1249@gmail.com>
2021-09-15 13:40:50 +0530

    styles and image in App.js

--- src/App.js
+++ src/App.js
@@ -20,9 +20,6 @@ class App extends React.Component {
   handleCountryChange = async (country) => {
     const fetchedData = await fetchData(country);
     this.setState({data : fetchedData, country: country});
-    console.log(country);
-    //fetch data
-    //set state
   }
 
   render(){
@@ -31,6 +28,7 @@ class App extends React.Component {
 
     return ( 
       <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange}/>
         <Chart data={data} country={country} />
